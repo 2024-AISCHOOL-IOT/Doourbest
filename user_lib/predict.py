9bbc31328e141a6c72f274125025ec1c57424779
JIS <jis3369@gmail.com>
2024-08-02 05:38:30 +0900

    last_version

--- user_lib/predict.py
+++ user_lib/predict.py
@@ -1,11 +1,11 @@
-from user_lib.preprocessing import return_dict as rd
+import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier # knn 분류 모델
 from sklearn.tree import DecisionTreeClassifier # tree 분류 모델
 
 _knn_model = None
 _tree_model = None
 
-merged_df = rd()
+merged_df = pd.read_csv('./data/refined.csv',encoding='utf-8')
 home_df = merged_df[['BLK_home', 'DREB_home', 'FG3_PCT_home', 'FG_PCT_home', 'FT_PCT_home', 'STL_home', 'TO_home']]
 away_df = merged_df[['BLK_away', 'DREB_away', 'FG3_PCT_away', 'FG_PCT_away', 'FT_PCT_away', 'STL_away', 'TO_away']]
 
@@ -28,9 +28,12 @@ def pre_knn(X):
         예측에 필요한 컬럼은\n
         ['BLK','DREB','FG3_PCT','FG_PCT','FT_PCT','STL','TO']\n
         결과 값은 홈팀의 우승여부
-        '''              
+        '''
+        print(X)
         if X is not None and _knn_model is not None:
-                return _knn_model.predict(X)
+                pre =  _knn_model.predict(X)
+                print(type(pre),pre)
+                return pre[0]
         else:
                 print("정상적인데이터를 넣어주세요")
                 return X
@@ -43,7 +46,20 @@ def pre_tree(X):
         결과 값은 홈팀의 우승여부
         '''
         if X is not None and _tree_model is not None:
-                return _tree_model.predict(X)
+                return _tree_model.predict(X)[0]
+        else:
+                print("정상적인데이터를 넣어주세요")
+                return X
+
+def pre_rf(X):
+        '''
+        데이터는 home팀기준 away팀과의 박스스코어 차이\n
+        예측에 필요한 컬럼은\n
+        ['BLK','DREB','FG3_PCT','FG_PCT','FT_PCT','STL','TO']\n
+        결과 값은 홈팀의 우승여부
+        '''
+        if X is not None and _rf_model is not None:
+                return _rf_model.predict(X)[0]
         else:
                 print("정상적인데이터를 넣어주세요")
                 return X
